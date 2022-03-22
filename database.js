7d1fb3ab13acc2928944cf10952eeab152479cb8
uliseshdez1 <101840829+uliseshdez1@users.noreply.github.com>
2022-03-21 18:35:03 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "soyelpechocho",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "b6935ab1fc7148",
+	password: "41604e58",
 	port: 3306,
-	database: "adoption_system",
+	database: "heroku_aa8887f33169378",
 	secret_key: "k60j9d9xzywqpPje"
 };
 exports.config = config;
