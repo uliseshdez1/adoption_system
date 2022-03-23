7301451d373dcb9f111e66e909b3c3e74a577c07
uliseshdez1 <101840829+uliseshdez1@users.noreply.github.com>
2022-03-23 15:41:13 -0600

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,7 +8,7 @@ controller.getLogin = (req, res) =>{
         id : req.body.id,
         time : new Date().getTime()
     };
-    const token = jtw.sign({user}, db.secret_key, {expiresIn:'1m'});
+    const token = jtw.sign({user}, db.secret_key, {expiresIn:'5m'});
     res.status(200);
     format.success = true;
     format.code = 200;
